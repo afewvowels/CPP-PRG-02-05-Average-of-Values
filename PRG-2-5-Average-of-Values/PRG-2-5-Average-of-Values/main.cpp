cb0072767a3a9be3e371522948eec04c5a4d808c
Keith Smith <aeiouandsometimesy@me.com>
2017-10-05 19:16:22 -0400

    Initial project creation

---
+++ PRG-2-5-Average-of-Values/PRG-2-5-Average-of-Values/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-2-5-Average-of-Values
+//
+//  Created by Keith Smith on 10/5/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
